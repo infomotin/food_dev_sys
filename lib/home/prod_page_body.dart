c952eb4c7ef560d78a34032642d30d5a5b75dba9
Abdul Motin <motin.mmk.jr@gmail.com>
2022-04-04 14:18:48 +0600

    index and rev and prv height

--- lib/home/prod_page_body.dart
+++ lib/home/prod_page_body.dart
@@ -17,6 +17,8 @@ class _ProdPageBodyState extends State<ProdPageBody> {
   var _currentPageValue = 0.0;
   //80% bigger than crrunt size
   double _scaleFacto = 0.8;
+  //get parrent height
+  double _height = 220;
 
   // initPage if in enter this page then do some thing
   @override
@@ -25,7 +27,7 @@ class _ProdPageBodyState extends State<ProdPageBody> {
     pageController.addListener(() {
       setState(() {
         _currentPageValue = pageController.page!;
-        print("Current Page Value is:" + _currentPageValue.toString());
+        // print("Current Page Value is:" + _currentPageValue.toString());
       });
     });
   }
@@ -56,108 +58,128 @@ class _ProdPageBodyState extends State<ProdPageBody> {
   //declear fun body
   Widget _buildPageProdItem(int index) {
     // create a instance matrix4 ?
-    Matrix4 matrix4 = new Matrix4.identity();
+    Matrix4 matrix = new Matrix4.identity();
     // if match current page value and index are same than got staple
     if (index == _currentPageValue.floor()) {
       var currScale = 1 - (_currentPageValue - index) * (1 - _scaleFacto);
-      print("currScale :" + currScale.toString());
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+      print(matrix.toString());
+      // print("currScale :" + currScale.toString());
     } else if (index == _currentPageValue.floor() + 1) {
       var currScale =
           _scaleFacto + (_currentPageValue - index + 1) * (1 - _scaleFacto);
-      print("currScale :" + currScale.toString());
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+      print(matrix.toString());
+      // print("currScale :" + currScale.toString());
+    } else if (index == _currentPageValue.floor() - 1) {
+      var currScale = 1 - (_currentPageValue - index) * (1 - _scaleFacto);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+      print(matrix.toString());
+      // print("currScale :" + currScale.toString());
     }
-    return Stack(
-      children: [
-        Container(
-          //create a first slider
-          height: 220,
-          margin: EdgeInsets.only(left: 15, right: 15),
-          decoration: BoxDecoration(
-              color: index.isEven ? Color(0xFFcccab0) : Color(0xFF92cab0),
-              borderRadius: BorderRadius.circular(30),
-              image: DecorationImage(
-                  fit: BoxFit.cover, image: AssetImage("image/prod2.png"))),
-        ),
-        //overlap container
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
             //create a first slider
-            height: 120,
-            margin: EdgeInsets.only(left: 30, right: 30, bottom: 30),
+            height: 220,
+            margin: EdgeInsets.only(left: 15, right: 15),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: Colors.white,
-            ),
+                color: index.isEven ? Color(0xFFcccab0) : Color(0xFF92cab0),
+                borderRadius: BorderRadius.circular(30),
+                image: DecorationImage(
+                    fit: BoxFit.cover, image: AssetImage("image/prod2.png"))),
+          ),
+          //overlap container
+          Align(
+            alignment: Alignment.bottomCenter,
             child: Container(
-              padding: EdgeInsets.only(top: 10, left: 15, right: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  BigText(text: "Chinese Side"),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    children: [
-                      Wrap(
-                        children: List.generate(
-                            5,
-                            (index) => Icon(
-                                  Icons.star,
-                                  color: Colors.red,
-                                  size: 15,
-                                )),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      SmallText(
-                        text: "4.5",
-                        color: Colors.black54,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      SmallText(text: "1254"),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      SmallText(
-                        text: "Comments.",
-                        color: Colors.black45,
-                      ),
-                    ],
-                  ),
-                  //create space
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    children: [
-                      TextAndIconWidget(
-                          icon: Icons.circle_sharp,
-                          text: "Normal",
-                          textColor: Colors.black45,
-                          iconColor: Colors.brown),
-                      TextAndIconWidget(
-                          icon: Icons.location_on,
-                          text: "1.7 Km",
-                          textColor: Colors.pink,
-                          iconColor: Colors.lightBlue),
-                      TextAndIconWidget(
-                          icon: Icons.access_time,
-                          text: "35 Minutes",
-                          textColor: Colors.red,
-                          iconColor: Colors.black45),
-                    ],
-                  )
-                ],
+              //create a first slider
+              height: 120,
+              margin: EdgeInsets.only(left: 30, right: 30, bottom: 30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+              ),
+              child: Container(
+                padding: EdgeInsets.only(top: 10, left: 15, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BigText(text: "Chinese Side"),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(
+                              5,
+                              (index) => Icon(
+                                    Icons.star,
+                                    color: Colors.red,
+                                    size: 15,
+                                  )),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(
+                          text: "4.5",
+                          color: Colors.black54,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "1254"),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(
+                          text: "Comments.",
+                          color: Colors.black45,
+                        ),
+                      ],
+                    ),
+                    //create space
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: [
+                        TextAndIconWidget(
+                            icon: Icons.circle_sharp,
+                            text: "Normal",
+                            textColor: Colors.black45,
+                            iconColor: Colors.brown),
+                        TextAndIconWidget(
+                            icon: Icons.location_on,
+                            text: "1.7 Km",
+                            textColor: Colors.pink,
+                            iconColor: Colors.lightBlue),
+                        TextAndIconWidget(
+                            icon: Icons.access_time,
+                            text: "35 Minutes",
+                            textColor: Colors.red,
+                            iconColor: Colors.black45),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
