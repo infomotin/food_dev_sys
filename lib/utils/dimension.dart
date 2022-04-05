6a3c6a0b65143cd59f6aa903adf9d163a31608db
Abdul Motin <motin.mmk.jr@gmail.com>
2022-04-05 13:59:48 +0600

    update roules on dinamic value

--- lib/utils/dimension.dart
+++ lib/utils/dimension.dart
@@ -1,8 +1,11 @@
 import 'package:get/get.dart';
 
-class Dimansions {
+class Dimensions {
   static double screenHeight = Get.context!.height;
   static double screenWidth = Get.context!.width;
-
-}
+  // static double factorialValueForContainer = screenHeight / 220;
+  // static double factorialValueForTextContainer = screenHeight / 220;
+  // use  heigth and width pageViewContainer
+  static double pageViewContainer = screenHeight / 3.48;
+  static double pageViewTextContainer = screenHeight / 7.03;
 }
